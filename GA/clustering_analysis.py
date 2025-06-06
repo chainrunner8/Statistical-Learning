0da5b449f539fd17d9e7d210b5049c4b4869c8e0
chainrunner8 <melkkior@outlook.com>
2025-06-06 18:39:10 +0200

    PCA done

--- GA/clustering_analysis.py
+++ GA/clustering_analysis.py
@@ -1,13 +1,26 @@
-from ucimlrepo import fetch_ucirepo
-
-import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
+from tabulate import tabulate
+from ucimlrepo import fetch_ucirepo 
+from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
-  
+
+
 # fetch dataset 
 wine_quality = fetch_ucirepo(id=186) 
-  
+
 # data (as pandas dataframes) 
 X = wine_quality.data.features 
 y = wine_quality.data.targets
+
+
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+pca_X = PCA()
+pca_X.fit(X_scaled)
+scores = pca_X.transform(X_scaled)
+
 
